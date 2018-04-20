f84fff1c9ff2543149bf09aa919a90e74548eff4
672664 <672664@01HW496881.India.TCS.com>
2018-04-20 19:55:09 +0530

    Changes@Aditya

--- FileIt/src/main/webapp/app/modules/header/controller/HeaderController.js
+++ FileIt/src/main/webapp/app/modules/header/controller/HeaderController.js
@@ -1,5 +1,9 @@
 fileItApp.controller('HeaderController', [ '$rootScope', '$scope', '$location',
 		'$sessionStorage',
 		function($rootScope, $scope, $location, $sessionStorage) {
+	
+	$scope.backtoHome = function() {
+		$location.path('/home');
+	}
 
 		} ]);
